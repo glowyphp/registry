77928c548f60d4f003359877653e3e2bea5f0e87
Awilum <awilum@msn.com>
2021-02-20 12:58:54 +0300

    Move to PHP 7.4; Extend Atomastic Arrays; Code refactoring; Update tests

--- src/Registry.php
+++ src/Registry.php
@@ -7,21 +7,17 @@ namespace Atomastic\Registry;
 use Atomastic\Arrays\Arrays;
 use Exception;
 
-final class Registry
+class Registry extends Arrays
 {
     /**
      * Registry instance
-     *
-     * @var Registry
      */
-    private static $instance = null;
+    private static ?Registry $instance = null;
 
     /**
      * Registry storage
-     *
-     * @var Arrays
      */
-    private static $storage = null;
+    private static ?Arrays $storage = null;
 
     /**
      * Gets the instance via lazy initialization (created on first usage)
@@ -45,7 +41,6 @@ final class Registry
      */
     protected function __construct()
     {
-
     }
 
     /**
@@ -53,82 +48,13 @@ final class Registry
      */
     protected function __clone()
     {
-        
     }
 
     /**
      * Prevent from being unserialized (which would create a second instance of it)
      */
-    public function __wakeup()
+    public function __wakeup(): void
     {
-        throw new Exception("Cannot unserialize a Registry.");
-    }
-
-    /**
-     * Set an registry data to a given value using "dot" notation.
-     *
-     * If no key is given to the method, the entire registry data will be replaced.
-     *
-     * @param  string $key   Key
-     * @param  mixed  $value Value
-     */
-    public function set(string $key, $value): self
-    {
-         static::$storage->set($key, $value);
-
-         return $this;
-    }
-
-    /**
-     * Determine if the registry has a value for the given name.
-     *
-     * @param  string|array $keys The keys of the registry item to check for existence.
-     */
-    public function has($keys): bool
-    {
-        if (static::$storage->has($keys)) {
-            return true;
-        }
-
-        return false;
-    }
-
-    /**
-     * Get item from the registry.
-     *
-     * @param  string $key     The keys of the registry item to get.
-     * @param  mixed  $default Default value
-     */
-    public function get(string $key, $default = null)
-    {
-        return static::$storage->get($key, $default);
-    }
-
-    /**
-     * Delete a items from the registry.
-     *
-     * @param  array|string $keys Keys.
-     */
-    public function delete($keys): self
-    {
-        static::$storage->delete($keys);
-
-        return $this;
-    }
-
-    /**
-     * Flush all items from the registry.
-     */
-    public function flush(): void
-    {
-        static::$storage = null;
-    }
-
-    /**
-     * Get all items from the registry.
-     */
-    public function all(): array
-    {
-        return static::$storage->all();
+        throw new Exception('Cannot unserialize a Registry.');
     }
 }
--- tests/RegistryTest.php
+++ tests/RegistryTest.php
@@ -5,51 +5,18 @@ declare(strict_types = 1);
 use Atomastic\Registry\Registry;
 
 test('test getInstance() method', function() {
-    $this->assertEquals(Registry::getInstance(), Registry::getInstance());
+    $this->assertInstanceOf(Registry::class, Registry::getInstance());
 });
 
 test('test registry() helper', function() {
     $this->assertEquals(Registry::getInstance(), registry());
+    $this->assertInstanceOf(Registry::class, registry());
 });
 
-test('test set() method', function() {
-    Registry::getInstance()->set('movies.the-thin-red-line.title', 'The Thin Red Line');
+test('test registry uniqueness', function() {
+    $firstCall = Registry::getInstance();
+    $secondCall = Registry::getInstance();
 
-    $this->assertEquals('The Thin Red Line',
-                        Registry::getInstance()->get('movies.the-thin-red-line.title'));
-});
-
-test('test get() method', function() {
-    Registry::getInstance()->set('movies.the-thin-red-line.title', 'The Thin Red Line');
-
-    $this->assertEquals('The Thin Red Line',
-                        Registry::getInstance()->get('movies.the-thin-red-line.title'));
-});
-
-test('test has() method', function() {
-    $this->assertTrue(Registry::getInstance()->has('movies.the-thin-red-line.title'));
-    $this->assertFalse(Registry::getInstance()->has('movies.the-thin-red-line.description'));
-});
-
-test('test all() method', function() {
-    $this->assertEquals(['movies' => ['the-thin-red-line' => ['title' => 'The Thin Red Line']]],Registry::getInstance()->all());
-});
-
-test('test delete() method', function() {
-    Registry::getInstance()->delete('movies.the-thin-red-line.title');
-
-    $this->assertFalse(Registry::getInstance()->has('movies.the-thin-red-line.title'));
-});
-
-test('test flush() method', function() {
-    Registry::getInstance()->set('movies.the-thin-red-line.title', 'The Thin Red Line')
-                           ->set('movies.the-thin-red-line.description', 'Lorem ipsum dolor sit amet');
-
-    $this->assertTrue(Registry::getInstance()->has('movies.the-thin-red-line'));
-    $this->assertEquals(null, Registry::getInstance()->flush());
-
-    Registry::getInstance()->set('movies.the-thin-red-line.title', 'The Thin Red Line')
-                           ->set('movies.the-thin-red-line.description', 'Lorem ipsum dolor sit amet');
-
-    $this->assertTrue(Registry::getInstance()->has('movies.the-thin-red-line'));
+    $this->assertInstanceOf(Registry::class, $firstCall);
+    $this->assertSame($firstCall, $secondCall);
 });
